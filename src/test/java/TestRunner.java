dfa590651e2ef26c514a3e689189848be3e1efa3
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-11-04 10:51:54 -0400

    Added Config.properties and fixed testrunner
    Added Config.properties and fixed testrunner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,13 +1,18 @@
+import com.vimalselvam.cucumber.listener.Reporter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+
+import java.io.File;
 
 @CucumberOptions(
         features = "src/test/java/features",
         glue = {"hooks", "runners", "stepdefinitions"},
         tags = "@Regression",
-        plugin = {"html:target/cucumber-reports/cucumber-pretty",
+        plugin = {"html:target/cucumber-reports/cucumber-pretty.html",
                 "json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"},
         monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
+
 }
