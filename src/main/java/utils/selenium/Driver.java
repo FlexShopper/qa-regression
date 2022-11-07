50f8d87adc6f10f880b7cc8fa805abb29b222924
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-11-07 12:02:20 -0500

    Removed old framework structure

--- src/main/java/utils/selenium/Driver.java
+++
@@ -1,9 +0,0 @@
-package utils.selenium;
-
-import org.openqa.selenium.WebDriver;
-
-public class Driver {
-    public static WebDriver browser() {
-        return DriverController.instance.webDriver;
-    }
-}
