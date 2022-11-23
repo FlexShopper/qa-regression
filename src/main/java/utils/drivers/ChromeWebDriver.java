0703e94a3a6fe4eb89cd9a1342ae049c936440c2
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-11-23 17:30:47 -0500

    Update ChromeWebDriver.java

--- src/main/java/utils/drivers/ChromeWebDriver.java
+++ src/main/java/utils/drivers/ChromeWebDriver.java
@@ -19,7 +19,7 @@ public class ChromeWebDriver {
         ChromeDriverService driverService = ChromeDriverService.createDefaultService();
 
         // Running Chrome in Headless mode to solve error: "DevToolsActivePort file doesn't exist"
-        ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions().setHeadless(true);
         // Following arguments were added to solve error: "DevToolsActivePort file doesn't exist"
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-setuid-sandbox");
