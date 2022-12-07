68ee6948987f3f524102b0322337685ab1832255
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-07 13:16:20 -0500

    Fixes for ExtentReport

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,3 +1,4 @@
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerAdapter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Listeners;
@@ -9,7 +10,7 @@ import org.testng.annotations.Listeners;
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "json:target/cucumber-reports/CucumberTestReport.json"},
         monochrome = true)
-@Listeners()
+@Listeners({ExtentITestListenerAdapter.class})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
