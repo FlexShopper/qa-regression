5f94315c6d8c092eca0218bf6052ed0fb721895d
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-13 16:08:53 -0500

    Changed Tags

--- src/test/java/PP3TestRunner.java
+++ src/test/java/PP3TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = {"hooks", "stepdefinitions"},
-        tags = "@Web",
+        tags = "@PP3",
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"},
