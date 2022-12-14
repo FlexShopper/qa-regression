f2474e2339b283df3847db59b15ae215c7d4ede9
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-14 16:41:37 -0500

    Commit changes

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = {"hooks", "stepdefinitions"},
-        tags = "@Web and @Decision",
+        tags = "@Web",
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"},
