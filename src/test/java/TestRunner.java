4a7da67dba710813bbd2431f656e1e68449f2450
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-15 12:40:53 -0500

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
         features = "src/test/java/features",
         glue = {"hooks", "stepdefinitions"},
         tags = "@PP3",
-        plugin = {"html:target/cucumber-reports/cucumber-pretty",
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"},
         monochrome = true)
