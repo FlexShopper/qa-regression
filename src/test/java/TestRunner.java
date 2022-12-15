eb09009456ffc984e6a4693c491de3d6495ac387
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-15 15:44:45 -0500

    rolled back to the initial pom.xml config

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,8 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
         features = "src/test/java/features",
         glue = {"hooks", "stepdefinitions"},
         tags = "@Email",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "json:target/cucumber-reports/CucumberTestReport.json",
+        plugin = {"json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"},
         monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
