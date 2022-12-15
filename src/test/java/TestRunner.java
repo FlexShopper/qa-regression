cd64238e324a450a79f6d80d3efa600d2e61abac
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-14 22:36:53 -0500

    pom.xml & TestRunner updates

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,11 +1,11 @@
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
         features = "src/test/java/features",
         glue = {"hooks", "stepdefinitions"},
-        tags = "@PP3",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        tags = "@Email",
+        plugin = {"html:target/cucumber-html-report",
                 "json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"},
         monochrome = true)
