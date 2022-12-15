32ba9ba17c05d8da3c28167730b02247782902f7
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-15 14:22:42 -0500

    removed multithread support for now

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,5 @@
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/java/features",
@@ -11,9 +10,5 @@ import org.testng.annotations.DataProvider;
                 "rerun:target/cucumber-reports/rerun.txt"},
         monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
+
 }
