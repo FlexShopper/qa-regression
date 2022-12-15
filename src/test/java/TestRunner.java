ac04811cfdafd1eb0696071a9ff8fff2a12c1091
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2022-12-15 14:12:15 -0500

    added support for Parallel execution via testng

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,5 +1,6 @@
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/java/features",
@@ -10,5 +11,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
                 "rerun:target/cucumber-reports/rerun.txt"},
         monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
-
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
