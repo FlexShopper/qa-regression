2e2f17bedadafdbc76f677cb3f7a87a5e5c19454
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2023-01-04 14:41:29 -0500

    Running tag PP3

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/features",
         glue = {"hooks", "stepdefinitions"},
-        tags = "@Decision",
+        tags = "@PP3",
         plugin = {"html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/CucumberTestReport.json",
                 "rerun:target/cucumber-reports/rerun.txt"})
