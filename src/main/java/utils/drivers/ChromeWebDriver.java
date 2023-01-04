88315da31d20e034cf76a9a5ca3b0e1231deb025
navasflexshopper <104596443+navasflexshopper@users.noreply.github.com>
2023-01-04 12:41:37 -0500

    Update ChromeWebDriver.java

--- src/main/java/utils/drivers/ChromeWebDriver.java
+++ src/main/java/utils/drivers/ChromeWebDriver.java
@@ -23,7 +23,7 @@ public class ChromeWebDriver {
         // Chrome needs to run in Headless mode in the Jenkins CI to solve error: "DevToolsActivePort file doesn't exist"
         ChromeOptions options = new ChromeOptions();
         // Following arguments were added to solve error: "DevToolsActivePort file doesn't exist"
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-setuid-sandbox");
         options.addArguments("--disable-dev-shm-usage");
